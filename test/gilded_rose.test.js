d991148bbd08a5c011a26054113b36730c4ec340
YAMEOGO Abdoul Moubarak <moubarak.webmaster@gmail.com>
2021-12-17 12:25:29 +0100

    Rajout d'un filet de tests qui enregistre le comportement de l’application

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,31 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
+const items = [
+  new Item("+5 Dexterity Vest", 10, 20),
+  new Item("Aged Brie", 2, 0),
+  new Item("Elixir of the Mongoose", 5, 7),
+  new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+
+  // This Conjured item does not work properly yet
+  new Item("Conjured Mana Cake", 3, 6),
+];
+
+const days = Number(process.argv[2]) || 2;
+const gildedRose = new Shop(items);
+const shopBehaviour = [];
+
 describe("Gilded Rose", function() {
   it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    
+    for (let day = 0; day < days; day++) {
+      shopBehaviour.push(JSON.parse(JSON.stringify(gildedRose.items)));
+      gildedRose.updateQuality();
+    }
+
+    expect(shopBehaviour).toMatchSnapshot();
   });
 });
