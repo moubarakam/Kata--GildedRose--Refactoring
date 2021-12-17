bd1eecf95f425040a05440cf70929ec9893167a2
YAMEOGO Abdoul Moubarak <moubarak.webmaster@gmail.com>
2021-12-17 12:49:56 +0100

    On garde le test (on va tester le code sur une  semaine), tout en considérant que le comportement du code initial est la vérité

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -3,6 +3,7 @@ const {Shop, Item} = require("../src/gilded_rose");
 const items = [
   new Item("+5 Dexterity Vest", 10, 20),
   new Item("Aged Brie", 2, 0),
+  new Item("Aged Brie", 2, 49),
   new Item("Elixir of the Mongoose", 5, 7),
   new Item("Sulfuras, Hand of Ragnaros", 0, 80),
   new Item("Sulfuras, Hand of Ragnaros", -1, 80),
@@ -14,15 +15,16 @@ const items = [
   new Item("Conjured Mana Cake", 3, 6),
 ];
 
-const days = Number(process.argv[2]) || 2;
+const days = Number(process.argv[7]) || 7;
 const gildedRose = new Shop(items);
 const shopBehaviour = [];
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
+const cloneBehavior = JSON.parse(JSON.stringify(gildedRose.items));
+describe("Gilded Rose Refactoring ###################################################", function() {
+  it("We consider that the behavior of the initial code is the truth", function() {
     
     for (let day = 0; day < days; day++) {
-      shopBehaviour.push(JSON.parse(JSON.stringify(gildedRose.items)));
+      shopBehaviour.push(cloneBehavior);
       gildedRose.updateQuality();
     }
 
